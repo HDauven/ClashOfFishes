52da0377bbe67c841661a2421ddc4c0fecc91782
Hein Dauven <heindauven@gmail.com>
2015-10-11 20:44:37 +0200

    Setup of Multi-threading research project
    Initial setup of the Multi-threading research project.

---
+++ multithreading-onderzoek/src/multithreading/onderzoek/MultithreadingOnderzoek.java
@@ -0,0 +1,16 @@
+package multithreading.onderzoek;
+
+/**
+ * Test setup for the Multi-threading research assignment.
+ * @author Hein Dauven & Stef Philips
+ */
+public class MultithreadingOnderzoek {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
