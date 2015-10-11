4520a33329636f949d6e31af76d1c4c4a6a7f494
Hein Dauven <heindauven@gmail.com>
2015-10-11 21:06:42 +0200

    ThreadClass initial setup

---
+++ multithreading-onderzoek/src/multithreading/onderzoek/ThreadClass.java
@@ -0,0 +1,31 @@
+package multithreading.onderzoek;
+
+/**
+ *
+ * @author Hein
+ */
+public class ThreadClass extends Thread {
+
+    /**
+     * 
+     */
+    ThreadClass() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    /**
+     * 
+     */
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    /**
+     * 
+     */
+    @Override
+    public void start() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }     
+}
