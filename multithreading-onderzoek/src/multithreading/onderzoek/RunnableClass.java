d8674d5b456bd0f203e96fe7035dfbcccf872d2e
Hein Dauven <heindauven@gmail.com>
2015-10-11 21:05:01 +0200

    RunnableClass initial setup

---
+++ multithreading-onderzoek/src/multithreading/onderzoek/RunnableClass.java
@@ -0,0 +1,30 @@
+package multithreading.onderzoek;
+
+/**
+ * RunnableClass test
+ * @author Hein Dauven
+ */
+public class RunnableClass implements Runnable {
+
+    /**
+     * 
+     */
+    RunnableClass() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    /**
+     * 
+     */
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    /**
+     * 
+     */
+    public void start() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }   
+}
