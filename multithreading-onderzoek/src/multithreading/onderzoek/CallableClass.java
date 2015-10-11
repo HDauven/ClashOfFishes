4704ab22794cdf4a370461962afca24e12aec8bf
Hein Dauven <heindauven@gmail.com>
2015-10-11 21:33:36 +0200

    ForkJoinClass initial setup

--- multithreading-onderzoek/src/multithreading/onderzoek/CallableClass.java
+++ multithreading-onderzoek/src/multithreading/onderzoek/CallableClass.java
@@ -16,6 +16,5 @@ public class CallableClass implements Callable<Long> {
     @Override
     public Long call() throws Exception {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
+    }   
 }
---
+++ multithreading-onderzoek/src/multithreading/onderzoek/ForkJoinClass.java
@@ -0,0 +1,25 @@
+package multithreading.onderzoek;
+
+import java.util.concurrent.RecursiveAction;
+
+/**
+ * ForkJoinClass test
+ * @author Hein Dauven
+ */
+public class ForkJoinClass extends RecursiveAction {
+
+    /**
+     * 
+     */
+    public ForkJoinClass() {
+        
+    }
+    
+    /**
+     * 
+     */
+    @Override
+    protected void compute() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }    
+}
