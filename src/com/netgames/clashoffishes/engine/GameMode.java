8b223213ea63ad47897b1c19772eed1f1baf2cd6
Hein Dauven <heindauven@gmail.com>
2015-10-12 15:00:13 +0200

    GameModi names fixed

--- src/com/netgames/clashoffishes/engine/GameMode.java
+++ src/com/netgames/clashoffishes/engine/GameMode.java
@@ -4,7 +4,7 @@ package com.netgames.clashoffishes.engine;
  * Created by bram on 1/10/15.
  */
 public enum GameMode {
-    LAST_FISH_STADNING,
-    REVOLTION_OF_TIME,
+    LAST_FISH_STANDING,
+    EVOLUTION_OF_TIME,
     EVOLVED
 }
