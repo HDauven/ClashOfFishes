6a50fb5a5defa72ef57154d4a6b4667698f7aa86
StefPhilipsen <s.philipsen@student.fontys.nl>
2015-10-12 16:05:45 +0200

    Eerste tests toegevoegd

---
+++ test/com/netgames/clashoffishes/data/DatabaseStorageTest.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.netgames.clashoffishes.data;
+
+import com.netgames.clashoffishes.User;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Stef
+ */
+public class DatabaseStorageTest
+{    
+    @Before
+    public void setUp()
+    {
+    }
+    
+    @After
+    public void tearDown()
+    {
+    }
+
+    /**
+     * Test of addUser method, of class DatabaseStorage.
+     */
+    @Test
+    public void testAddUser()
+    {
+        
+    }
+
+    /**
+     * Test of getUser method, of class DatabaseStorage.
+     */
+    @Test
+    public void testGetUser()
+    {
+        
+    }
+
+    /**
+     * Test of logIn method, of class DatabaseStorage.
+     */
+    @Test
+    public void testLogIn()
+    {
+        
+    }
+    
+}
