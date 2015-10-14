c562bd4eab6d1d8f578140993ab3dcf7d7e07d8c
StefPhilipsen <s.philipsen@student.fontys.nl>
2015-10-14 12:16:56 +0200

    Tests toegevoegd

--- test/com/netgames/clashoffishes/data/FileStorageTest.java
+++ test/com/netgames/clashoffishes/data/FileStorageTest.java
@@ -9,6 +9,7 @@ import com.netgames.clashoffishes.User;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -16,15 +17,15 @@ import static org.junit.Assert.*;
  *
  * @author Stef
  */
-public class DatabaseStorageTest
+public class FileStorageTest
 {
 
-    DatabaseStorage dbStorage;
+    FileStorage fileStorage;
 
     @Before
     public void setUp()
     {
-        dbStorage = new DatabaseStorage();
+        fileStorage = new FileStorage();
     }
 
     @After
@@ -44,10 +45,10 @@ public class DatabaseStorageTest
     @Test
     public void testAddUser()
     {
-        dbStorage.addUser(new User("Henk", "Henk@Henk.nl"), "wachtwoord");
-        dbStorage.addUser(new User("Piet", "Piet@hotmail.com", 16), "wachtwoord");
-        dbStorage.addUser(new User("henk", "Henkie@Henkie.nl"), "wachtwoord1");// Mag niet worden toegevoegd, username bestaat al in db
-        dbStorage.addUser(new User("henkie", "henk@henk.nl"), "wachtwoord");//Mag niet worden toegevoegd, email bestaat al in db
+        fileStorage.addUser(new User("Henk", "Henk@Henk.nl"), "wachtwoord");
+        fileStorage.addUser(new User("Piet", "Piet@hotmail.com", 16), "wachtwoord");
+        fileStorage.addUser(new User("henk", "Henkie@Henkie.nl"), "wachtwoord1");// Mag niet worden toegevoegd, username bestaat al in db
+        fileStorage.addUser(new User("henkie", "henk@henk.nl"), "wachtwoord");//Mag niet worden toegevoegd, email bestaat al in db
     }
 
     /**
@@ -56,12 +57,12 @@ public class DatabaseStorageTest
     @Test
     public void testGetUser()
     {        
-        assertNull(dbStorage.getUser("henkie"));
-        User henk = dbStorage.getUser("henk");
+        assertNull(fileStorage.getUser("henkie"));
+        User henk = fileStorage.getUser("henk");
         assertNotNull("User Henk moet bestaan.", henk);
         assertEquals("Score moet 0 zijn. ", henk.getScore());
         assertEquals("Email moet 'Henk@Henk.nl' zijn.", "Henk@Henk.nl", henk.getEmail());
-        User piet = dbStorage.getUser("Piet");
+        User piet = fileStorage.getUser("Piet");
         assertEquals("Score moet 16 zijn.", 16, piet.getScore());
     }
 
@@ -71,11 +72,11 @@ public class DatabaseStorageTest
     @Test
     public void testLogIn()
     {
-        assertNull(dbStorage.logIn("henk", "verkeerdWW"));
-        assertNull(dbStorage.logIn("henk@henk.nl", "verkeerdWW"));
-        assertNull(dbStorage.logIn("henk", "wachtwoord1"));
-        assertNull(dbStorage.logIn("henk", "WACHTWOORD"));
-        assertNotNull(dbStorage.logIn("henk", "wachtwoord"));
-        assertNotNull(dbStorage.logIn("henk@henk.nl", "wachtwoord"));
+        assertNull(fileStorage.logIn("henk", "verkeerdWW"));
+        assertNull(fileStorage.logIn("henk@henk.nl", "verkeerdWW"));
+        assertNull(fileStorage.logIn("henk", "wachtwoord1"));
+        assertNull(fileStorage.logIn("henk", "WACHTWOORD"));
+        assertNotNull(fileStorage.logIn("henk", "wachtwoord"));
+        assertNotNull(fileStorage.logIn("henk@henk.nl", "wachtwoord"));
     }
 }
