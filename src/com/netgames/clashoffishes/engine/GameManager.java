87a80c0f1cf45b5b5c2f9179c1352047ace81990
Hein Dauven <heindauven@gmail.com>
2015-10-19 11:34:03 +0200

    GameManager cleanup
    Changed the setup of the GameManager.
    
    The GameManager is now its own window.

--- src/com/netgames/clashoffishes/engine/GameManager.java
+++ src/com/netgames/clashoffishes/engine/GameManager.java
@@ -1,20 +1,52 @@
 package com.netgames.clashoffishes.engine;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
 /**
- * Created by bram on 1/10/15.
+ * Class that manages the game itself.
+ * 
+ * @author Hein Dauven
  */
-public class GameManager {
+public class GameManager extends Application {
+    static final double WIDTH = 640, HEIGHT = 400;
+    private boolean up, down, left, right;
+    private boolean wKey, aKey, sKey, dKey;
+    private Scene scene;
+    private StackPane root;
+    
     private final GameMode gameMode;
+    private GameLoop gamePlayLoop;
+    private ObjectManager castDirector;
+    
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("InvinciBagel");
+        root = new StackPane();
+        scene = new Scene(root, WIDTH, HEIGHT, Color.WHITE);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        
+        createSceneEventHandling();
+        loadAudioAssets();
+        loadImageAssets();
+        createGameObject();
+        addGameObjectNodes();
+        createObjectManager();
+        createSplashScreenNodes();
+        addNodesToStackPane();
+        createStartGameLoop();
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
 
     public GameManager(GameMode gameMode) {
         this.gameMode = gameMode;
-    }
-
-    private void generateWorld() {
-
-    }
-
-    private void checkGameState() {
-
     }
 }
