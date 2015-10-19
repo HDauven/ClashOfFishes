92ce1e2b3269e95a5b6610c32924432d2d3455ae
Hein Dauven <heindauven@gmail.com>
2015-10-19 10:23:24 +0200

    Game Object constructor + fields
    Added the new Game Object constructors and the fields that belong to it.

--- src/com/netgames/clashoffishes/engine/object/GameObject.java
+++ src/com/netgames/clashoffishes/engine/object/GameObject.java
@@ -1,5 +1,12 @@
 package com.netgames.clashoffishes.engine.object;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.SVGPath;
+
 /**
  * GameObject
  * abstract class that functions as the basic implementation for
@@ -10,11 +17,43 @@ package com.netgames.clashoffishes.engine.object;
  * @version %I%, %G%
  */
 public abstract class GameObject {
+    protected List<Image> imageStates = new ArrayList<>();
+    protected ImageView spriteFrame;
+    protected SVGPath spriteBound;
+    protected double iX;
+    protected double iY;
+    protected double pX;
+    protected double pY;
+    protected boolean isAlive;
+    protected boolean isFixed;
+    protected boolean isBonus;
+    protected boolean hasValue;
+    protected boolean isFlipV;
+    protected boolean isFlipH;
+    
     /**
      * The constructor of a GameObject object.
+     * @param SVGData
+     * @param xLocation
+     * @param yLocation
+     * @param spriteCels
      */
-    public GameObject() {
-        
+    public GameObject(String SVGData, double xLocation, 
+                        double yLocation, Image... spriteCels) {
+        this.spriteBound = new SVGPath();
+        this.spriteBound.setContent(SVGData);
+        this.spriteFrame = new ImageView(spriteCels[0]);
+        this.imageStates.addAll(Arrays.asList(spriteCels));
+        this.iX = xLocation;
+        this.iY = yLocation;
+        this.pX = 0;
+        this.pY = 0;
+        this.isAlive = false;
+        this.isFixed = false;
+        this.isBonus = false;
+        this.hasValue = false;
+        this.isFlipV = false;
+        this.isFlipH = false;
     }
 
     /**
