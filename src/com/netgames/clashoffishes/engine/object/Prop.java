466209aba72be3f424bd423442d0f6c8c8fd71b2
Hein Dauven <heindauven@gmail.com>
2015-10-19 15:43:36 +0200

    Created Prop class
    This class will be used in combination with static game objects that are
    going to be displayed as is.

---
+++ src/com/netgames/clashoffishes/engine/object/Prop.java
@@ -0,0 +1,22 @@
+package com.netgames.clashoffishes.engine.object;
+
+import javafx.scene.image.Image;
+
+/**
+ * Static game objects that will be used as-is.
+ *
+ * @author Hein Dauven
+ */
+public class Prop extends GameObject {
+
+    public Prop(String SVGData, double xLocation, double yLocation, Image... spriteCels) {
+        super(SVGData, xLocation, yLocation, spriteCels);
+        spriteFrame.setTranslateX(xLocation);
+        spriteFrame.setTranslateY(yLocation);
+    }
+
+    @Override
+    public void update() {
+        /* not implemented yet */
+    }   
+}
