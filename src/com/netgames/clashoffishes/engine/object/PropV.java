671e034ea50254d8abb2d7915d636462a7536fe5
Hein Dauven <heindauven@gmail.com>
2015-10-19 15:49:01 +0200

    Created PropV class
    This class will be used in combination with static game objects that are
    going to be flipped around the Y axis.

---
+++ src/com/netgames/clashoffishes/engine/object/PropV.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.netgames.clashoffishes.engine.object;
+
+/**
+ *
+ * @author Hein
+ */
+public class PropV {
+    
+}
