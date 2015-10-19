1b816dbca62e116417b2801acc05fff770b3894f
Hein Dauven <heindauven@gmail.com>
2015-10-19 11:35:53 +0200

    Added GameManager event handling

--- src/com/netgames/clashoffishes/engine/GameManager.java
+++ src/com/netgames/clashoffishes/engine/GameManager.java
@@ -2,6 +2,7 @@ package com.netgames.clashoffishes.engine;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -44,6 +45,38 @@ public class GameManager extends Application {
     
     public static void main(String[] args) {
         launch(args);
+    }
+    
+    /**
+     *  
+     */
+    private void createSceneEventHandling() {
+        scene.setOnKeyPressed((KeyEvent event) -> {
+            switch (event.getCode()) {
+                case UP:    up    = true; break;
+                case DOWN:  down  = true; break;
+                case LEFT:  left  = true; break;
+                case RIGHT: right = true; break;
+                case W:     wKey  = true; break;
+                case A:     aKey  = true; break;
+                case S:     sKey  = true; break;
+                case D:     dKey  = true; break;    
+            }
+            
+        });
+        
+        scene.setOnKeyReleased((KeyEvent event) -> {
+            switch (event.getCode()) {
+                case UP:    up    = false; break;
+                case DOWN:  down  = false; break;
+                case LEFT:  left  = false; break;
+                case RIGHT: right = false; break;
+                case W:     wKey  = false; break;
+                case A:     aKey  = false; break;
+                case S:     sKey  = false; break;
+                case D:     dKey  = false; break;    
+            }
+        });
     }
 
     public GameManager(GameMode gameMode) {
