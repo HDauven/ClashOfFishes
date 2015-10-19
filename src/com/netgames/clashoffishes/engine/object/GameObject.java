7645a8dff7fc6ae5392fcb3aa772d5092719d0b3
Hein Dauven <heindauven@gmail.com>
2015-10-19 10:20:45 +0200

    GameObject cleanup

--- src/com/netgames/clashoffishes/engine/object/GameObject.java
+++ src/com/netgames/clashoffishes/engine/object/GameObject.java
@@ -1,58 +1,24 @@
 package com.netgames.clashoffishes.engine.object;
 
-import com.netgames.clashoffishes.engine.GameMode;
-import com.netgames.clashoffishes.engine.math.Vector2;
-import javafx.scene.canvas.GraphicsContext;
-
 /**
  * GameObject
  * abstract class that functions as the basic implementation for
  * all the GameObjects that are available in the game.
+ * This class will primarily be used for fixed sprites.
  * 
- * @author Bram Hoendervangers
  * @author Hein Dauven
- * @version 1.0
+ * @version %I%, %G%
  */
 public abstract class GameObject {
-    /** The position of a GameObject */
-    private Vector2 position;
-    /** The velocity of a GameObject */
-    private Vector2 velocity;
-
     /**
      * The constructor of a GameObject object.
-     * @param x x coordinate of the GameObject object.
-     * @param y y coordinate of the GameObject object.
      */
-    public GameObject(int x, int y) {
-        position = new Vector2(x, y);
-        velocity = new Vector2(0, 0);
+    public GameObject() {
+        
     }
 
     /**
      * The update method updates the position of the GameObject.
      */
     protected abstract void update();
-
-    /**
-     * The render method renders the GameObject on its actual position.
-     * @param ctx draw call for the GameObject.
-     */
-    protected abstract void render(GraphicsContext ctx);
-
-    /**
-     * Gets the current position of the GameObject.
-     * @return position data of the GameObject
-     */
-    public Vector2 getPosition() {
-        return position;
-    }
-
-    /**
-     * Get the current velocity of the GameObject.
-     * @return velocity data of the GameObject
-     */
-    public Vector2 getVelocity() {
-        return velocity;
-    }
 }
