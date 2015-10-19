6f466642c7aeeb16b24958cf143b6178e99a327c
Hein Dauven <heindauven@gmail.com>
2015-10-19 11:10:26 +0200

    Created the GameLoop
    Created the GameLoop class and added basic functionality to it.

---
+++ src/com/netgames/clashoffishes/engine/GameLoop.java
@@ -0,0 +1,32 @@
+package com.netgames.clashoffishes.engine;
+
+import javafx.animation.AnimationTimer;
+
+/**
+ * The game play loop.
+ * 
+ * @author Hein Dauven
+ */
+public class GameLoop extends AnimationTimer {
+    protected GameManager gameManager;
+
+    public GameLoop(GameManager manager) {
+        gameManager = manager;
+    }
+    
+    @Override
+    public void handle(long now) {
+        // TODO
+        // gamemanaer.player.update();
+    }
+    
+    @Override
+    public void start() {
+        super.start();
+    } 
+    
+    @Override
+    public void stop() {
+        super.stop();
+    }
+}
