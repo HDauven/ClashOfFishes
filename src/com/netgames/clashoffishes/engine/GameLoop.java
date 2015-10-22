91141509bec19352853cae9c33ab4ade4f65aeee
Hein Dauven <heindauven@gmail.com>
2015-10-22 23:09:17 +0200

    Added GameLoop javadoc

--- src/com/netgames/clashoffishes/engine/GameLoop.java
+++ src/com/netgames/clashoffishes/engine/GameLoop.java
@@ -8,23 +8,39 @@ import javafx.animation.AnimationTimer;
  * @author Hein Dauven
  */
 public class GameLoop extends AnimationTimer {
+    /* A reference to the GameManager class. */
     protected GameManager gameManager;
 
+    /**
+     * Constructor for the GameLoop.
+     * @param manager GameManager class reference.
+     */
     public GameLoop(GameManager manager) {
         gameManager = manager;
     }
     
+    /**
+     * This method is going to be called in every frame while the AnimationTimer is active.
+     * Capped at 60 frames per second by default.
+     * @param now The timestamp of the current frame given in nanoseconds. This value will be the same for all AnimationTimers called during one frame.
+     */
     @Override
     public void handle(long now) {
         // TODO
         gameManager.getPlayer().update();
     }
     
+    /**
+     * Starts the AnimationTimers. Once it is started, the handle(long) method of this AnimationTimers will be called in every frame.
+     */
     @Override
     public void start() {
         super.start();
     } 
     
+    /**
+     * Stops the AnimationTimers. 
+     */
     @Override
     public void stop() {
         super.stop();
