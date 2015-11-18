f8e7b97e890d85f65202fc9dc794e0d10693e0d3
StefPhilipsen <s.philipsen@student.fontys.nl>
2015-11-18 22:08:01 +0100

    Singleplayer automatically selects first fish

--- src/com/netgames/clashoffishes/util/GuiUtilities.java
+++ src/com/netgames/clashoffishes/util/GuiUtilities.java
@@ -19,42 +19,74 @@ import javafx.stage.Stage;
  *
  * @author MuK
  */
-public class GuiUtilities {
-    
+public class GuiUtilities
+{
+
     //TODO GuiTitles aanvullen
     public static final String GAME_WORLD_TITLE = "Clash of Fishes!";
-    public static final String HIGHSCORE_TITLE = "Highscores.";  
+    public static final String HIGHSCORE_TITLE = "Highscores.";
     public static final String HOSTED_GAMES_TITLE = "Hosted Games.";
-    public static final String LOGIN_TITLE = "Welcome - Please login using your username/email & password.";   
+    public static final String LOGIN_TITLE = "Welcome - Please login using your username/email & password.";
     public static final String REGISTRATION_TITLE = "Register a new account.";
     public static final String CHARACTER_SELECTION_TITLE = "Select your character!";
-    
+
     //Object window = controller of sender
-    public static void buildStage(Object window, String fileName, String title) {
-        try {
-                Stage stage = (Stage) window;
-                FXMLLoader loader = new FXMLLoader(GuiUtilities.class.getResource("/com/netgames/clashoffishes/ui/" + fileName + ".fxml"));
-                Parent root = (Parent) loader.load();
-                stage.setTitle(title);
-                stage.setScene(new Scene(root));
-                stage.show();
-            } catch (IOException ex) {
-                Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
-            }
+    public static void buildStage(Object window, String fileName, String title)
+    {
+        try
+        {
+            Stage stage = (Stage) window;
+            FXMLLoader loader = new FXMLLoader(GuiUtilities.class.getResource("/com/netgames/clashoffishes/ui/" + fileName + ".fxml"));
+            Parent root = (Parent) loader.load();
+            stage.setTitle(title);
+            stage.setScene(new Scene(root));
+                        
+            stage.show();
+        }
+        catch (IOException ex)
+        {
+            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
-    public static String getMainMenusTitle() {
+
+    public static String getMainMenusTitle()
+    {
         Administration administration = Administration.get();
         String username = "";
-        if (administration.getLoggedInUser() != null) {
+        if (administration.getLoggedInUser() != null)
+        {
             username = administration.getLoggedInUser().getUsername();
         }
         return "Welcome back: " + username;
-    } 
-    
-    public static String getFishPoolTitle() {
+    }
+
+    public static String getFishPoolTitle()
+    {
         Administration administration = Administration.get();
         String username = administration.getCurrentLobby().getUsers().get(0).getUsername() + "'s Lobby";
         return username;
+    }
+
+    public static void logOut(Stage stage)
+    {
+        stage.close();
+        //Stage stage = (Stage) window;
+        FXMLLoader loader = new FXMLLoader(GuiUtilities.class.getResource("/com/netgames/clashoffishes/ui/" + "Login" + ".fxml"));
+        Parent root;
+        try
+        {
+            root = (Parent) loader.load();
+            stage.setTitle("Login");
+            stage.setScene(new Scene(root));
+            stage.show();
+        }
+        catch (IOException ex)
+        {
+            Logger.getLogger(GuiUtilities.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public static void closeApplication(Stage stage){
+        stage.close();
     }
 }
