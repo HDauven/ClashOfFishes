3280330b247cdd7936a1d62af3e4e49e390a7e70
Hein Dauven <heindauven@gmail.com>
2015-11-23 23:55:37 +0100

    Added remote interfaces to the game

---
+++ src/com/netgames/clashoffishes/server/remote/IServer.java
@@ -0,0 +1,27 @@
+package com.netgames.clashoffishes.server.remote;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+
+/**
+ * Interface that makes the defined methods available for users of the IServer.
+ * @author Hein Dauven
+ */
+public interface IServer extends Remote {
+    /**
+     * Method that registers an ILobby to the IServer.
+     * @param lobby The ILobby that is going to be added to the IServer.
+     * @throws RemoteException 
+     */
+    void registerLobby(ILobby lobby) 
+            throws RemoteException;
+    
+    /**
+     * Method that gets all the ILobbies that are registered with the IServer.
+     * @return returns a list of messages to the client.
+     * @throws RemoteException 
+     */
+    List<ILobby> listChatServers()
+            throws RemoteException;    
+}
