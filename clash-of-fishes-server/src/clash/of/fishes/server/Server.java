d21c949799ee550ef8479c60c6764a77c10b8569
Hein Dauven <heindauven@gmail.com>
2015-11-23 22:11:22 +0100

    Implemented the Server class

--- clash-of-fishes-server/src/clash/of/fishes/server/Server.java
+++ clash-of-fishes-server/src/clash/of/fishes/server/Server.java
@@ -1,16 +1,38 @@
 package clash.of.fishes.server;
 
+import clash.of.fishes.server.remote.ILobby;
+import clash.of.fishes.server.remote.IServer;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
- *
+ * Class that keeps track of all the lobbies that exist of this game.
  * @author Hein Dauven
  */
-public class Server extends UnicastRemoteObject {
+public class Server extends UnicastRemoteObject implements IServer {
+    private static final long serialVersionUID = 1L;
+    
+    private List<ILobby> lobbies;
+    
     /**
      * Constructor for the server
      * @throws RemoteException 
      */
-    public Server() throws RemoteException {}
+    public Server() throws RemoteException {
+        super();
+        lobbies = new ArrayList<>();
+    }
+
+    @Override
+    public void registerLobby(ILobby lobby) throws RemoteException {
+        lobbies.add(lobby);
+    }
+
+    @Override
+    public List<ILobby> listChatServers() throws RemoteException {
+        return Collections.unmodifiableList(lobbies);
+    }
 }
