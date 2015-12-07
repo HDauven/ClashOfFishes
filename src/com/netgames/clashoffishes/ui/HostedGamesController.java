9ae641b09c0874eee62f0387a4762e3bd1797685
Hein Dauven <heindauven@gmail.com>
2015-12-07 21:07:35 +0100

    Revert "Removed comments"
    This reverts commit ef071f5e9dbcbac92d3d4698daba5faccc6f6099.

--- src/com/netgames/clashoffishes/ui/HostedGamesController.java
+++ src/com/netgames/clashoffishes/ui/HostedGamesController.java
@@ -85,7 +85,18 @@ public class HostedGamesController implements Initializable {
                 Logger.getLogger(HostedGamesController.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
+//        try {
+//            for (ILobby lobby : this.cofServer.listLobbies()) {
+//
+//            }
+//        } catch (RemoteException remoteException) {
+//            System.out.println(remoteException.getMessage());
+//        }
+
+        //ObservableList<Lobby> lobbies = FXCollections.observableArrayList(lobby, lobby, lobby, lobby);
         tbvHostedGames.setItems(lobbies);
+        //tbvHostedGames.getColumns().addAll(clmPoolName, clmPlayers, clmGameMode);
+        //TODO Aan lobby werken
     }
 
     @FXML
