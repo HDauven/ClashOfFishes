f8452b0e9e6a11d786be5eef3dd828ac050a7860
StefPhilipsen <s.philipsen@student.fontys.nl>
2015-12-11 16:31:56 +0100

    IPadresses changed

--- src/com/netgames/clashoffishes/server/LobbyRegistry.java
+++ src/com/netgames/clashoffishes/server/LobbyRegistry.java
@@ -45,7 +45,7 @@ public class LobbyRegistry extends Observable
     GameServer gameServer = null;
 
     private static IServer cofServer;
-    private final String cofServerURL = "rmi://145.93.173.5:1100/Server";
+    private final String cofServerURL = "rmi://192.168.200.38:1100/Server";
     
     // Constructor
     public LobbyRegistry()
