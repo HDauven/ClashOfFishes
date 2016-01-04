1d6d4d75d89b1429ad8d6699764419ec0faf1160
Hein Dauven <heindauven@gmail.com>
2016-01-04 15:24:50 +0100

    Added new utility classes

---
+++ src/com/netgames/clashoffishes/util/HandlerUtilities.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2015, 2016, Clash of Fishes and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ * Further input required:
+ */
+package com.netgames.clashoffishes.util;
+
+/**
+ * Holds Handler Utility methods.
+ * @author Hein Dauven
+ */
+public class HandlerUtilities {
+    
+}
---
+++ src/com/netgames/clashoffishes/util/RMIUtilities.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2015, 2016, Clash of Fishes and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ * Further input required:
+ */
+package com.netgames.clashoffishes.util;
+
+/**
+ * Holds RMI method calls.
+ * @author Hein Dauven
+ */
+public class RMIUtilities {
+    
+}
